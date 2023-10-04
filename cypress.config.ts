fc7a446955e91ff15c901469bfd2196946fb90c9
Hamza Bounasseh <Hamza.Bounasseh@gmail.com>
2023-10-04 13:23:02 +0100

    Implement first test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  
+
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: 'http://localhost:4200',
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    experimentalStudio: true,
+    video: true,
   },
-  
-  
+
+
   component: {
     devServer: {
       framework: 'angular',
@@ -14,5 +18,5 @@ export default defineConfig({
     },
     specPattern: '**/*.cy.ts'
   }
-  
+
 })
