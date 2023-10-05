02f35e61f7bcca631e5152d0372c87c889867e98
Hamza Bounasseh <Hamza.Bounasseh@gmail.com>
2023-10-05 04:56:11 +0100

    Modify baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 
   e2e: {
-    baseUrl: 'http://localhost:4200',
     viewportWidth: 1366,
     viewportHeight: 768,
     experimentalStudio: true,
