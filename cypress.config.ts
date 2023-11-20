4efa61b6fa0990a82fab1810dcd438eb426f3a0c
Hamza Bounasseh <Hamza.Bounasseh@gmail.com>
2023-11-20 21:10:02 +0100

    npm i cypress-mochawesome-reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
 
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     viewportWidth: 1366,
     viewportHeight: 768,
     experimentalStudio: true,
     video: true,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 
 
